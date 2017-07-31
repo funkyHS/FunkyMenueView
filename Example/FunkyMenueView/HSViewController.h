b080730d18430dc45c03075d4b0bb847d05f8d1c
funkyHS <hs1024942667@163.com>
2017-07-31 16:09:36 +0800

    Initial commit

---
+++ Example/FunkyMenueView/HSViewController.h
@@ -0,0 +1,13 @@
+//
+//  HSViewController.h
+//  FunkyMenueView
+//
+//  Created by funkyHS on 07/31/2017.
+//  Copyright (c) 2017 funkyHS. All rights reserved.
+//
+
+@import UIKit;
+
+@interface HSViewController : UIViewController
+
+@end
---
+++ Example/FunkyMenueView/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FunkyMenueView
+//
+//  Created by funkyHS on 07/31/2017.
+//  Copyright (c) 2017 funkyHS. All rights reserved.
+//
+
+@import UIKit;
+#import "HSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HSAppDelegate class]));
+    }
+}
